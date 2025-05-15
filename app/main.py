c7dbd64568156f89e6992df5d56353a6c564c8bd
kaushalrijal <kaushalrijal091@gmail.com>
2025-05-16 03:03:12 +0545

    add now route

--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timezone
+
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
@@ -13,6 +15,15 @@ templates = Jinja2Templates(directory="app/templates")
 @app.get("/", response_class=HTMLResponse)
 async def root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.get("/now")
+def get_current_timestamp():
+    now_utc = datetime.now(timezone.utc)
+    unix_time = int(now_utc.timestamp())
+    utc_time = now_utc.isoformat()
+
+    return {"utc": utc_time, "unix": unix_time}
 
 
 app.include_router(api_router, prefix="/api")
